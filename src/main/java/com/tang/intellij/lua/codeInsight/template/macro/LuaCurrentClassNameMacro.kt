1f9cb0aaac9dfe0a996719a6b0aead6b66c25d5a
teddyboy2004 <teddyboy2004@163.com>
2023-09-22 10:12:17 +0800

    1. 优化当前类名模型支持函数外使用

--- src/main/java/com/tang/intellij/lua/codeInsight/template/macro/LuaCurrentClassNameMacro.kt
+++ src/main/java/com/tang/intellij/lua/codeInsight/template/macro/LuaCurrentClassNameMacro.kt
@@ -20,16 +20,23 @@ import com.intellij.codeInsight.lookup.LookupElement
 import com.intellij.codeInsight.lookup.LookupElementBuilder
 import com.intellij.codeInsight.template.*
 import com.intellij.psi.PsiFile
+import com.intellij.psi.util.PsiTreeUtil
+import com.intellij.psi.util.childrenOfType
 import com.tang.intellij.lua.codeInsight.template.context.LuaFunContextType
-import com.tang.intellij.lua.psi.*
+import com.tang.intellij.lua.psi.LuaAssignStat
+import com.tang.intellij.lua.psi.LuaClassMethodDefStat
+import com.tang.intellij.lua.psi.LuaLocalDefStat
+import com.tang.intellij.lua.psi.guessType
+import com.tang.intellij.lua.search.SearchContext
+import com.tang.intellij.lua.ty.TyClass
+import com.tang.intellij.lua.ty.TyTable
 
 class LuaCurrentClassNameMacro : Macro() {
     override fun getPresentableName() = "LuaCurrentClassName()"
 
     override fun getName() = "LuaCurrentClassName"
 
-    fun removeFunction(classMethodName: String): String
-    {
+    fun removeFunction(classMethodName: String): String {
         return classMethodName.replace(Regex("[.:].*"), "")
     }
 
@@ -49,14 +56,56 @@ class LuaCurrentClassNameMacro : Macro() {
 
     override fun calculateLookupItems(params: Array<out Expression>, context: ExpressionContext?): Array<LookupElement>? {
         var e = context?.psiElementAtStartOffset
+        val srcElement = e
         val list = mutableListOf<LookupElement>()
         while (e != null && e !is PsiFile) {
             e = e.parent
             when (e) {
-                is LuaClassMethodDefStat ->
-                    {
-                        var classMethodName = e.classMethodName.text
-                        list.add(LookupElementBuilder.create(removeFunction(classMethodName)))
+                is LuaClassMethodDefStat -> {
+                    var classMethodName = e.classMethodName.text
+                    list.add(LookupElementBuilder.create(removeFunction(classMethodName)))
+                }
+            }
+        }
+        // 补充在函数外的情况
+        if (list.isEmpty() && srcElement != null) {
+            // 前一级是文件节点，向上找函数定义
+            if (srcElement.parent is PsiFile) {
+                e = srcElement.prevSibling
+                while (e != null) {
+                    when (e) {
+                        is LuaClassMethodDefStat -> {
+                            val classMethodName = e.classMethodName.text
+                            val lookupString = removeFunction(classMethodName)
+                            if (lookupString.isNotBlank()) {
+                                list.add(LookupElementBuilder.create(lookupString))
+                            }
+                            break
+                        }
+                    }
+                    e = e.prevSibling
+                }
+            }
+            // 还是找不到就找第一个 local xxx = {}
+                val searchContext = SearchContext.get(srcElement.project)
+            if (list.isEmpty()) {
+
+                val declarations = srcElement.containingFile.childrenOfType<LuaLocalDefStat>().filter { stat->stat.exprList?.guessType(searchContext) is TyClass }
+                declarations.forEach {
+                    if (it.comment?.tagClass != null) {
+                        list.add(LookupElementBuilder.create(it.localDefList.first().name))
+                    }
+                }
+                if (list.isEmpty() && declarations.isNotEmpty())
+                {
+                    list.add(LookupElementBuilder.create(declarations.first().localDefList.first().name))
+                }
+            }
+            // 还是没有就找第一个xxx = {}
+            if (list.isEmpty()) {
+                val stat = PsiTreeUtil.getChildrenOfType(srcElement.containingFile, LuaAssignStat::class.java)?.filter { stat-> stat.valueExprList?.guessType(searchContext) is TyClass }
+                if (!stat.isNullOrEmpty()) {
+                    list.add(LookupElementBuilder.create(stat.first().varExprList.firstChild.text))
                 }
             }
         }
