570ef1a32fd4e143c0c7c048607e4b2ba5cc75b9
teddyboy2004 <teddyboy2004@163.com>
2023-09-26 15:05:34 +0800

    1. 支持跳转类型定义

---
+++ src/main/java/com/tang/intellij/lua/editor/LuaTypeDeclarationProvider.kt
@@ -0,0 +1,64 @@
+/*
+ * Copyright (c) 2017. tangzx(love.tangzx@qq.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.tang.intellij.lua.editor
+
+import com.intellij.codeInsight.navigation.actions.TypeDeclarationPlaceAwareProvider
+import com.intellij.openapi.editor.Editor
+import com.intellij.psi.PsiElement
+import com.tang.intellij.lua.psi.LuaPsiTypeGuessable
+import com.tang.intellij.lua.search.SearchContext
+import com.tang.intellij.lua.stubs.index.LuaAliasIndex
+import com.tang.intellij.lua.stubs.index.LuaClassIndex
+import com.tang.intellij.lua.ty.TyAlias
+import com.tang.intellij.lua.ty.TyClass
+
+/**
+ * Goto Symbol
+ * Created by TangZX on 2016/12/12.
+ */
+class LuaTypeDeclarationProvider : TypeDeclarationPlaceAwareProvider {
+    override fun getSymbolTypeDeclarations(p0: PsiElement, p1: Editor?, p2: Int): Array<PsiElement>? {
+        if (p0 is LuaPsiTypeGuessable) {
+            val project = p0.project
+            val context = SearchContext.get(project)
+            val type = p0.guessType(context)
+            if (type is TyClass) {
+                val className = type.className
+                val ty = type.resolve(context)
+                if (ty is TyAlias)
+                {
+                    val alias = LuaAliasIndex.find(context, className)
+                    if (alias != null) {
+                        return arrayOf(alias)
+                    }
+                }
+                else
+                {
+                    val clazz = LuaClassIndex.find(context, className)
+                    if (clazz != null) {
+                        return arrayOf(clazz)
+                    }
+                }
+            }
+        }
+        return null
+    }
+
+    override fun getSymbolTypeDeclarations(p0: PsiElement): Array<PsiElement>? {
+        return getSymbolTypeDeclarations(p0, null, -1)
+    }
+}
